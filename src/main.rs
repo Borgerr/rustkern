3bf32055e9ea1a72456c0730a567b1a998eb78f0
Ashton Hunt <ashkernel02@gmail.com>
2024-06-30 14:13:48 -0600

    VGA printing

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,25 @@
 #![no_std]
 #![no_main]
 
+use core::fmt::Write;
+mod vga_buffer;
+
 // panic handler function required
 use core::panic::PanicInfo;
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {  // should never return; returns never `!` type
+fn panic(info: &PanicInfo) -> ! {
+    // should never return; returns never `!` type
+    println!("Oops!: {}", info);
     loop {}
 }
-
-static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     // entry point
     // linker looks for function named `_start` by default
-    //
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("hullo :]");
+    println!("{} plus {} is {}, minus {} that's {} quick maffs", 2, 2, 4, 1, 3);
+    //panic!();
 
     loop {}
 }
-
